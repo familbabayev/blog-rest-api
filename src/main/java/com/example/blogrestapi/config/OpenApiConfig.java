b4e6d95904757c628ce0319bb21ad872d6ab8244
Famil Babayev <famil.babayev.eng@gmail.com>
2024-02-11 12:39:44 +0200

    updated swagger to have jwt authentication

--- src/main/java/com/example/blogrestapi/config/OpenApiConfig.java
+++ src/main/java/com/example/blogrestapi/config/OpenApiConfig.java
@@ -1,14 +1,36 @@
 package com.example.blogrestapi.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
         info = @Info(
                 title = "Blog REST API",
                 version = "v1.0.0",
                 description = "Blog REST API using Spring Boot"
-        )
+        ),
+        servers = {
+                @Server(
+                        url = "http://localhost:8080",
+                        description = "Local server"
+                ),
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT Bearer Token. 'Bearer' prefix is already included, paste the token without the prefix.",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT"
 )
 public class OpenApiConfig {
 
